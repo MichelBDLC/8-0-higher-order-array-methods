c380f1e1d2317d7774513be44266dc999f404926
MichelBDLC <michelbdlc@gmail.com>
2022-12-13 17:15:57 -0800

    all done

--- src/06-sort.js
+++ src/06-sort.js
@@ -13,27 +13,51 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByRuntimeAscending(songs) {}
+function sortByRuntimeAscending(songs) {
+  function compareRuntime(a, b) {
+    return a.runtimeInSeconds - b.runtimeInSeconds
+  }
+  return songs.sort(compareRuntime)
+}
 
 /**
- * Reorders the array so that the song objects are organized by their artist name. The artist that comes last in the alphabet should come first.
+ * Reorders the array so that the song objects are organized by their artist name. The artist that comes last in the alphabet should come
+ * first.
  *
  * TIP: "A" and "a" sort differently. To avoid this issue, you may want to use `.toLowerCase()` or `.toUpperCase()`.
  *
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByArtistNameDescending(songs) {}
+function sortByArtistNameDescending(songs) {
+
+  return songs.sort((a, b) => {
+    let firstArtist = a.artist.toLowerCase()
+    let secondArtist = b.artist.toLowerCase()
+    if (firstArtist < secondArtist) {
+      return 1
+    }
+    else {return -1}
+  })
+}
 
 /**
- * Reorders the array so that the song objects are organized by their song title. The title that comes first in the alphabet should come first.
+ * Reorders the array so that the song objects are organized by their song title. The title that comes first in the alphabet should come 
+ * first.
  *
  * TIP: "A" and "a" sort differently. To avoid this issue, you may want to use `.toLowerCase()` or `.toUpperCase()`.
  *
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortBySongTitleAscending(songs) {}
+function sortBySongTitleAscending(songs) {
+
+  return songs.sort((a, b) => {
+    if (a.title.toLowerCase() < b.title.toLowerCase()) {
+      return -1
+    }
+  }) 
+}
 
 module.exports = {
   sortByRuntimeAscending,
